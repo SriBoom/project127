dc46de3dad327fd881770e8d398abfe470acb410
SriBoom <74517999+SriBoom@users.noreply.github.com>
2022-01-20 16:52:09 -0800

    Add files via upload

--- scrap.py
+++ scrap.py
@@ -1,50 +1,40 @@
-from email import header
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import time
-import csv
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
 
-START_URL= 'https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars'
-browser=webdriver.Chrome("chromedriver.exe")
-browser.get(START_URL)
-time.sleep(10)
 
-def scrape():
-    header=[
-        "vmag",
-        "name",
-        "bayer_designation",
-        "distance",
-        "spectral_class",
-        "mass",
-        "radius",
-        "lumonisity"
-    ]
-    star_data=[]
+bright_stars_url = 'https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars'
 
-    for i in range(0, 428):
-        soup = BeautifulSoup(browser.page_source, "html.parser")
-        for tr_tag in soup.find_all("tr", attrs={"class", "expoplanet"}):
-            th_tags = tr_tag.find_all("th")
-            temp_list = []
-            # enumerate is a counter. It will count how many object there are. Inside a li function if there are 4 data, it will show 4.
-            for index, th_tag in enumerate(th_tags):
-                if index == 0:
-                    temp_list.append(th_tag.find_all("a")[0].contents[0])
-                else:
-                    try:
-                        temp_list.append(th_tag.contents[0])
-                    except:
-                        temp_list.append("")
-            star_data.append(temp_list)
-            # xpath is used to navigate through elements or pages.
-            browser.find_element_by_xpath(
-                '//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a'
-            ).click()
+page = requests.get(bright_stars_url)
+print(page)
 
-with open("data.csv", "w") as f:
-    csvwriter=csv.writer(f)
-    csvwriter.writerow(header)
-    csvwriter.writerows(star_data)
+soup = bs(page.text,'html.parser')
+
+star_table = soup.find('table')
+
+temp_list= []
+table_rows = star_table.find_all('tr')
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+
+
+
+Star_names = []
+Distance =[]
+Mass = []
+Radius =[]
+Lum = []
+
+for i in range(1,len(temp_list)):
+    Star_names.append(temp_list[i][1])
+    Distance.append(temp_list[i][3])
+    Mass.append(temp_list[i][5])
+    Radius.append(temp_list[i][6])
+    Lum.append(temp_list[i][7])
     
-scrape()
+df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,Lum)),columns=['Star_name','Distance','Mass','Radius','Luminosity'])
+print(df2)
+
+df2.to_csv('bright_stars.csv')
